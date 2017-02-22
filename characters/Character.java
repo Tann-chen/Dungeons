e5f19e655c3ae63e13c1f7fa6e63a24f50809e63
Tann <t.e.chen@techendembp.wireless.concordia.ca>
2017-02-22 11:33:49 -0500

    architectural structure of characters

--- characters/Character.java
+++ characters/Character.java
@@ -1,17 +1,18 @@
 package characters;
 
+import Archive.Archivable;
 import items.Equipment;
+import org.dom4j.Element;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * only code the aspect of ability score
+ * character architectural structure
  */
-public class Character {
+public class Character implements Archivable{
 
      /*Ability score*/
-
     private int strength=0;
     private int dexterity=0;
     private int constitution=0;
@@ -20,13 +21,26 @@ public class Character {
     private int charisma=0;
 
     /*other values*/
-
+    private int level=0;
     private int hitPoint=0;
     private int armorClass=0;
     private int originalAttack=0;
     private int originalDamage=0;
 
+    private Character(){
+        modifier = new AbilityModifier(this);
+        wornEquipments=new ArrayList<Equipment>();
+        //TODO:这里面需要用到initAbilityScore()
+    }
+    /*这个方法用于设定人物的属性值，但是需要做安全性判断，当输入的属性值类型不存在时需要System.out.println报错*/
+    public void setAbilityScore(String abilityType, int value){
+        //TODO
+    }
 
+    /*这个函数是用在初始化人物的时候用的，按照要求在人物初始化的时候，人物的6项ability score 是随机决定的，但总值是固定的18*/
+    public void initAbilityScore(){
+        //TODO：这里面需要用到setAbilityScore()
+    }
 
     /*AbilityModifier*/
     /**
@@ -35,56 +49,52 @@ public class Character {
      * 例外：在人物编辑页面中，对人物能力的修改，在此页面中修改（人物原始能力）
      */
     private AbilityModifier modifier;
+
+    //TODO:这一块需要需求分析，如果兴趣去边看D20边写就写着，不然就先放着把
+
+
+    /* Worn Equipments */
+
     private List<Equipment> wornEquipments;
-
-    private Character(){
-        modifier = new AbilityModifier(this);
-        wornEquipments=new ArrayList<Equipment>();
-    }
-
-    /*getters*/
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public int getDexterity() {
-        return dexterity;
-    }
-
-    public int getConstitution() {
-        return constitution;
-    }
-
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public int getWisdom() {
-        return wisdom;
-    }
-
-    public int getCharisma() {
-        return charisma;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    public int getArmorClass() {
-        return armorClass;
-    }
-
-    public int getOriginalAttack() {
-        return originalAttack;
-    }
-
-    public int getOriginalDamage() {
-        return originalDamage;
-    }
 
     public List<Equipment> getWornEquipments() {
         return wornEquipments;
     }
+
+    public void wearingEquipment(Equipment equip){
+        //TODO:这是一个穿装备的动作
+        //TODO:一个人物身上只能穿一样同种类的装备，例如只能拿一把武器。当拿另一把武器时，要放弃本来拿的武器
+        //TODO:List<Equipement>去保存身上的武器可能不是最适宜，因为不好去确定身上是否有什么装备－> map()或者set()我仍在考虑中
+    }
+
+
+
+    /*因为在.xml存档中，人物身上穿的装备要随人物的信息一起存进去所以，穿在身上的装备
+    * 存档为.xml数的一个element*/
+    public Element encodeWornEquipments(){
+        //TODO
+        return null;
+         }
+
+    public void decodeWornEquipments(Element element){
+
+        //TODO :解析穿上身上装备的element后，将装备一件件读出来，放到wornEquipments（ArrayList）中。
+    }
+
+
+
+    /*  Archiving a character */
+
+    @Override
+    public Element encode(){
+       //TODO
+        return null;
+    }
+
+    @Override
+    public void decode(Element element){
+        //TODO
+    }
+
+
 }
---
+++ characters/CharacterManager.java
@@ -0,0 +1,16 @@
+package characters;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Character architectural structure
+ */
+public class CharacterManager {
+
+   /*这个类是为了在游戏开始的时候将创建过的角色一次性从.xml文档中读入游戏中*/
+   /*参考EquipmentManager*/
+   /*EquipmentManager已经实现在游戏一开始的时候将创建过的武器从.xml读入游戏中*/
+}
